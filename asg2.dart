b3c904ecb405fd51594283baab0d1eb2d8d3138e
Muhammad Usman <96245189+usman008@users.noreply.github.com>
2022-03-10 21:36:21 +0500

    Add files via upload

---
+++ asg2.dart
@@ -0,0 +1,24 @@
+main() {
+  String rollNo = '22';
+
+  print(int.parse(rollNo));
+  //22
+
+  print(double.parse(rollNo));
+  //22.0
+
+  int age = 22;
+  double height = 5.7;
+  bool pass = true;
+
+  print(age.toString());
+  //22
+
+  print(height.toString());
+  //5.7
+
+  print(pass.toString());
+  //true
+
+  
+}
